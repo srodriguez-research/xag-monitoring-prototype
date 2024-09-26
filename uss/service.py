4544b21fad261e8a7d41064514da0c5f7248349c
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2024-09-26 10:09:23 +1000

    fix(monitor): error when executing behave while monitoring.

--- uss/service.py
+++ uss/service.py
@@ -8,14 +8,23 @@ from uss.tempo import get_trace, search_tempo
 
 
 def verify_trace(trace_file):
-    cmd = f"poetry run behave --no-capture -D trace={trace_file}"
-    logging.debug(f"Running behave as subprocess with commnad {cmd}")
-    s = subprocess.run(cmd, check=False)
-    return s
+    command = f"behave --no-capture -D trace={trace_file}"
+    workdir = os.getcwd()
+    logging.debug(f"Running behave as subprocess [{command=}, {workdir=}")
+    # s = subprocess.run(cmd, check=False)
+
+    result = subprocess.run(
+        command.split(),
+        cwd=workdir,
+        capture_output=True,  # Python >= 3.7 only
+        text=True,  # Python >= 3.7 only
+        check=False,
+    )
+    return result
 
 
 def monitor_traces():
-    store_dir = Path("target/traces_store")
+    store_dir = Path("data/traces_store")
     store_dir.mkdir(parents=True, exist_ok=True)
     SERV_NAME = "xag-test"
     traceql = "{" + f'resource.service.name="{SERV_NAME}"' + "}"
@@ -28,4 +37,6 @@ def monitor_traces():
             logging.debug(f"getting trace {trace_id}")
             with open(trace_store_path, "w") as fp:
                 json.dump(get_trace(trace_id), fp)
-                verify_trace(trace_store_path)
+                fp.close()
+
+            verify_trace(trace_store_path)
