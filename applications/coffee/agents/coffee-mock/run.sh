0e8aebba6aabf7e1a51218495d7473ea3f69fea6
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2024-10-04 01:20:27 +1000

    feat(agentmock): automatically download opentelementry javaagent.

--- applications/coffee/agents/coffee-mock/run.sh
+++ applications/coffee/agents/coffee-mock/run.sh
@@ -1,6 +1,7 @@
-export JAVA_TOOL_OPTIONS="-javaagent:../libs/opentelemetry-javaagent.jar"
-export OTEL_RESOURCE_ATTRIBUTES=service.name=coffee-mock
 
 mvn clean package
+
+export JAVA_TOOL_OPTIONS="-javaagent:./target/opentelemetry-javaagent.jar"
+export OTEL_RESOURCE_ATTRIBUTES=service.name=coffee-mock
 
 java -jar target/coffee-mock-0.1-SNAPSHOT.jar
