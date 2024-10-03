3841199fa61e858bc43e9b95358cd125e60096c1
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2024-10-04 00:52:41 +1000

    fix: java version to 17.

--- applications/coffee/agents/coffee-mock/run.sh
+++ applications/coffee/agents/coffee-mock/run.sh
@@ -2,4 +2,5 @@ export JAVA_TOOL_OPTIONS="-javaagent:../libs/opentelemetry-javaagent.jar"
 export OTEL_RESOURCE_ATTRIBUTES=service.name=coffee-mock
 
 mvn clean package
-java -jar ./target/coffee-mock-0.1-SNAPSHOT.jar
+
+java -jar target/coffee-mock-0.1-SNAPSHOT.jar
